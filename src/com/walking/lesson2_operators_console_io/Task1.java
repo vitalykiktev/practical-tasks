9eafd186bdaa994af94d27e578d618cbaf4c2e85
i5t4b3r <i5t4b3r@gmail.com>
2025-03-05 17:32:09 +0300

    end: lesson 2

--- src/com/walking/lesson2_operators_console_io/Task1.java
+++ src/com/walking/lesson2_operators_console_io/Task1.java
@@ -1,5 +1,7 @@
 package com.walking.lesson2_operators_console_io;
 
+import java.util.Scanner;
+
 /**
  * Введите с клавиатуры вещественные числа a и b. Выведите на экран результат выражения
  * c = b*a*(a + b)/(a2),         где a2 - a в квадрате
@@ -8,5 +10,16 @@ package com.walking.lesson2_operators_console_io;
  */
 public class Task1 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число а: ");
+        double a = in.nextDouble();
+
+        System.out.println("Введите число b: ");
+        double b = in.nextDouble();
+
+        in.close();
+
+        double c = b * a * (a + b) / (a * a);
+        System.out.println(c);
     }
 }
--- src/com/walking/lesson2_operators_console_io/Task2.java
+++ src/com/walking/lesson2_operators_console_io/Task2.java
@@ -1,5 +1,10 @@
 package com.walking.lesson2_operators_console_io;
 
+import com.sun.source.doctree.EscapeTree;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Введите с клавиатуры целые числа a и b.
  * Выведите на экран результат сравнения:
@@ -7,5 +12,16 @@ package com.walking.lesson2_operators_console_io;
  */
 public class Task2 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число a: ");
+        int a = in.nextInt();
+
+        System.out.println("Введите число b: ");
+        int b = in.nextInt();
+
+        in.close();
+
+        boolean result = a *  a * a > b * b;
+        System.out.println(result);
     }
 }
